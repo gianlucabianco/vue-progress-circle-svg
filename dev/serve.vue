da1a9e964d48230332cd8d519b102ce73055ba1b
gianlucabianco <ciao@gianlucabiaco.dev>
2021-04-17 12:54:26 +0200

    feat: package scaffolding for SFC

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import ProgressCircle from '@/progress-circle.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    ProgressCircle
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <progress-circle />
+  </div>
+</template>
